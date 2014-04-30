f5419c57b95c92416074f948ac645c656cc698de
Matt Buckley <mbuc91@gmail.com>
2014-04-30 04:02:17 +0000

    Updated AutoPiGui2 to update RPM and MPH of UI, play a single music file, and adjust volume based on RPM.

--- RPi/AutoPiGui2.py
+++ RPi/AutoPiGui2.py
@@ -1,10 +1,52 @@
 #!/usr/bin/env python
 
 import time
+import pygame
+#import RPi.GPIO as GPIO
 
 from obd_capture import OBD_Capture
 from PyQt4 import QtGui, QtCore
 from AutoPi import Ui_AutoPi
+
+#GPIO.setmode(GPIO.BOARD)
+red = 12
+green = 16
+freq = 100 #Hz
+
+playing = 0
+paused  = 0
+
+#GPIO.setup(red, GPIO.OUT)
+#GPIO.setup(green, GPIO.OUT)
+
+
+class WorkThread(QtCore.QThread):
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+ 
+    def run(self):
+#        for i in range(6):
+#            time.sleep(0.3) # artificial time delay
+#            self.emit( QtCore.SIGNAL('update(QString)'), "from work thread " + str(i) )
+        
+#Get RPM
+        while 1:
+            (name, value, unit) = obd.get_value(0x0C)
+            ui.lcdNumberRPM.display(value)
+            vol = value/2000
+            if vol > 1:
+                vol = 1
+
+            if playing:
+                pygame.mixer.music.set_volume(vol)
+                time.sleep(1)
+
+            (name, value, unit) = obd.get_value(0x0D)
+            ui.lcdNumberMPH.display(value)
+#            vol = int(value/60)
+#            pygame.mixer.music.set_volume(vol)
+            time.sleep(0.5)
+        return
 
 class AutoPi(Ui_AutoPi):
     def __init__(self):
@@ -14,6 +56,23 @@ class AutoPi(Ui_AutoPi):
     #    x = int(self.lineEditX.text())
     #    y = int(self.lineEditY.text())
     #    self.LineEditZ.setText(str(x + y))*/
+        self.pushButton_Play.clicked.connect(self.handlePlay)
+    def handlePlay(self):
+        if playing:
+            pygame.mixer.music.pause
+            playing = 0
+            paused = 1
+        elif paused:
+            pygame.mixer.music.unpause
+            playing = 1
+            paused = 0
+        else:
+            pygame.mixer.music.play()
+            playing = 1
+        return
+    def handlePause(self):
+        pygame.music.pause()
+        return
 
 if __name__ == '__main__':
     import sys
@@ -25,6 +84,10 @@ if __name__ == '__main__':
     print "Displaying GUI"
 #    AutoPi.showFullScreen()
     AutoPi.show()
+
+#init pygame
+    pygame.mixer.init()
+    pygame.mixer.music.load("/home/pi/mysong.mp3")
 
     print "Attempting to start OBD_Capture()"
     obd = OBD_Capture()
@@ -40,7 +103,7 @@ if __name__ == '__main__':
         print "Error: OBD Not Connected."
     else:
         print "Connected and ready."
-        while 1:
+        while 0:
 #Get Coolant Temperature
             (name, value, unit) = obd.get_value(0x05)
 #
@@ -52,4 +115,9 @@ if __name__ == '__main__':
             (name, value, unit) = obd.get_value(0x0F)
             time.sleep(1)
 
+    print "starting worker thread"
+    ui.lcdNumberRPM.display(10)
+    workthread = WorkThread()
+    workthread.start()
+    print "done"
     sys.exit(app.exec_())
